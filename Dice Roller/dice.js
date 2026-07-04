4ef56aa189dab765d9e008b2914b71670b9a6ffb
Rhodex15 <rhodesoluwatomisinbe@gmail.com>
2026-07-04 14:27:59 +0100

    First push my first project dice roller

---
+++ Dice Roller/dice.js
@@ -0,0 +1,17 @@
+
+
+function rollDice() {
+    const nb_dice = document.getElementById("diceCount").value;
+    const result = document.getElementById("result");
+    const diceContainer = document.getElementById("diceContainer");
+
+    const value = [];
+    const imgs = [];
+    for (let i = 0; i < nb_dice; i++) {
+        const randomValue = Math.floor(Math.random() * 6) + 1;
+        value.push(randomValue);
+        imgs.push(`<img src ="images/dice-six-faces-${randomValue}.svg" alt="Dice">`);
+    }
+    result.textContent = `You rolled: ${value.join(", ")}`;
+    diceContainer.innerHTML = imgs.join("");
+}
